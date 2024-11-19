b62d42620b584a6706cc3c3326c05cff131b965a
mahiswar <desireddy.mahi.2002@gmail.com>
2024-11-20 01:08:38 +0530

    Add files via upload

---
+++ script_tests/line_select_tests.py
@@ -0,0 +1,28 @@
+import unittest
+
+import base
+
+
+class Test(base.BaseScriptTest, unittest.TestCase):
+    command_line = "./scripts/line_select.py ${features}"
+    input_features = base.TestFile(
+        """0
+                                       1
+                                       1
+                                       0
+                                       1
+                                       0"""
+    )
+    input_stdin = base.TestFile(
+        """a
+                                    b
+
+                                    d
+                                    e
+                                    f"""
+    )
+    output_stdout = base.TestFile(
+        """b
+
+                                      e"""
+    )
